e09f5e65e3d781835ad5cb329c136a93c8248d23
Stefan Altmayer <stoeoef@gmail.com>
2016-06-24 11:25:45 +0200

    Added licensing information.

--- src/boundingvolume.rs
+++ src/boundingvolume.rs
@@ -1,3 +1,18 @@
+// Copyright 2016 The RTree Developers. For a full listing of the authors,
+// refer to the Cargo.toml file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use cgmath::{BaseNum, Vector2};
 use cgmath::conv::array2;
 use num::{Bounded, zero, cast};
