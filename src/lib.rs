113ce55c782adf3e652bb1d981542c9954762185
Stefan Altmayer <stoeoef@gmail.com>
2016-09-23 13:20:51 +0200

    Doc fixes

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,9 @@
 
 //! # Spade
 //! Spade (SPAtial DatastructurEs, obviously!) implements a few nifty datastructures optimized for spatial access operations.
-//! The first major datastructure is an n-dimensional r*-tree [wikipedia](https://en.wikipedia.org/wiki/R-tree) for efficient nearest-neighbor and point lookup queries.
-//! The second datastructures implements a 2D delaunay triangulation [wikipedia](https://en.wikipedia.org/wiki/Delaunay_triangulation) backed by an r-tree for fast 
-//! nearest neighbor lookup. The triangulation also implements natural neighbor interpolation [wikipedia](https://en.wikipedia.org/wiki/Natural_neighbor) 
+//! The first major datastructure is an n-dimensional r*-tree ([wikipedia](https://en.wikipedia.org/wiki/R-tree)) for efficient nearest-neighbor and point lookup queries.
+//! The second datastructures implements a 2D delaunay triangulation ([wikipedia](https://en.wikipedia.org/wiki/Delaunay_triangulation)) backed by an r-tree for fast 
+//! nearest neighbor lookup. The triangulation also implements natural neighbor interpolation ([wikipedia](https://en.wikipedia.org/wiki/Natural_neighbor)) 
 //! which offers a smooth interpolation in a delaunay triangulation.
 //! All classes are purely written in (safe) rust, the package currently supports vectors from the nalgebra and cgmath crates.
 //!
