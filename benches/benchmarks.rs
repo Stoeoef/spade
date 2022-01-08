de8334d9bde18f524353c86ed8dc7ac7a988a13c
Stefan Altmayer <stoeoef@gmail.com>
2022-01-08 12:45:41 +0100

    Configures benchmark settings for improved benchmark times.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use criterion::*;
 
 mod benchmark_utilities;
@@ -7,8 +9,9 @@ mod locate_benchmark;
 
 criterion_group! {
     name = benches;
-    config = Criterion::default();
-    targets = locate_benchmark::locate_benchmark,
+    config = Criterion::default().sample_size(50).warm_up_time(Duration::from_secs(1));
+    targets =
+    locate_benchmark::locate_benchmark,
     bulk_load_benchmark::bulk_load_benchmark,
     bulk_load_vs_incremental::bulk_load_vs_incremental_benchmark,
 }
